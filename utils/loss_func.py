5606b8b5129d6d03ae173030ae4667d17582c505
Minh Hung <leminhhung933@gmail.com>
2024-04-01 12:09:59 +0700

    loss-func

--- utils/loss_func.py
+++ utils/loss_func.py
@@ -7,20 +7,20 @@ from torchmetrics.functional import peak_signal_noise_ratio as tmf_psnr
 def mse(output, target):
     squared_diff = torch.square(output - target)
     mse = torch.mean(squared_diff)
-    return mse.item()
+    return mse
 
 
 def ssim(output, target):
     ssim_score = tmf_ssim(output, target)
-    return ssim_score.item()
+    return ssim_score
 
 
 def L1(output, target):
     abs_diff = torch.abs(output - target)
     l1_loss = torch.mean(abs_diff)
-    return l1_loss.item()
+    return l1_loss
 
 
 def psnr(output, target):
     psnr_score = tmf_psnr(output, target)
-    return psnr_score.item()
+    return psnr_score
