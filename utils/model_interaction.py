e2508ab026719a2a72e7978dfc85c1aea2e665e1
Minh Hung <leminhhung933@gmail.com>
2024-04-01 16:44:37 +0700

    model-interaction

--- utils/model_interaction.py
+++ utils/model_interaction.py
@@ -1,13 +1,29 @@
 import torch
 
+
 def save_model(PATH, model):
     torch.save(model, PATH)
+
 
 def read_model(PATH):
     return torch.load(PATH)
 
+
 def save_checkpoint(PATH, checkpoint_info):
     torch.save(checkpoint_info, PATH)
-    
-def read_checkpoint(PATH):
-    return torch.load(PATH)
+
+
+def init_model(model_params):
+    return model_params
+
+
+def load_checkpoint(filepath, model, optimizer=None):
+    checkpoint = torch.load(filepath)
+    model.load_state_dict(checkpoint['state_dict'])
+    if optimizer is not None:
+        optimizer.load_state_dict(checkpoint['optimizer'])
+    epoch = checkpoint['iter']
+    if optimizer is not None:
+        return model, optimizer, epoch
+    else:
+        return model, epoch
