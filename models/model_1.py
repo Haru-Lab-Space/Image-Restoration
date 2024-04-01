e0d1364caa3986047445a8906f5018846f8c2c55
Minh Hung <leminhhung933@gmail.com>
2024-04-01 12:09:20 +0700

    model-test

--- models/model_1.py
+++ models/model_1.py
@@ -1,7 +1,11 @@
 from torch import nn
 
+
 class Model_1(nn.Module):
     def __init__(self):
         super(Model_1, self).__init__()
+        self.conv_layer = nn.Conv3d(
+            in_channels=3, out_channels=3, kernel_size=3, stride=1, padding='same')
+
     def forward(self, x):
-        return x
+        return self.conv_layer(x)
